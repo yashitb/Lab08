117d778445b5429c80d98e7b35f51858957ccbac
yashitb <yashitbamba107@gmail.com>
2025-10-30 18:22:02 -0600

    hasTest

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -21,5 +21,12 @@ public class CustomList {
         return cities.contains(city);
      }
 
+    public void deleteCity(City city) {
+        cities.remove(city);
+    }
+
+    public int countCities() {
+        return cities.size();
+    }
 
 }
--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -8,10 +8,29 @@ public class CustomListTest {
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
-
         //This will fail initially because hasCity() doesn't exist
-
         assertTrue(list.hasCity(calgary));
     }
 
+//    @Test
+//    public void testDeleteCity() {
+//        CustomList list = new CustomList();
+//        City calgary = new City("Calgary", "AB");
+//        list.addCity(calgary);
+//
+//        list.deleteCity(calgary); // This will fail initially
+//        assertFalse(list.hasCity(calgary));
+//    }
+
+//    @Test
+//    public void testCountCities() {
+//        CustomList list = new CustomList();
+//        City calgary = new City("Calgary", "AB");
+//        City edmonton = new City("Edmonton", "AB");
+//
+//        list.addCity(calgary);
+//        list.addCity(edmonton);
+//
+//        assertEquals(2, list.countCities());
+//    }
 }
