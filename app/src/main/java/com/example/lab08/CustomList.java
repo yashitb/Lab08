b08ee600cb91b137a9ffb0e830adadcd3de4e802
yashitb <yashitbamba107@gmail.com>
2025-10-31 15:29:40 -0600

    testCountCities

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -25,8 +25,8 @@ public class CustomList {
         cities.remove(city);
     }
 
-//    public int countCities() {
-//        return cities.size();
-//    }
+    public int countCities() {
+        return cities.size();
+    }
 
 }
